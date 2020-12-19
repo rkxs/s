d1448e30215cda57e18d1d003f6d62071cbf35c0
liunian <liunian@kdd.com>
2020-12-20 07:54:14 +0800

    update

--- docker.sh
+++ docker.sh
@@ -240,6 +240,8 @@ install_docker() {
 docker_run() {
 
   docker run -itd --name centosaiguo -p 80:80 -p 443:443 --privileged=true registry.cn-beijing.aliyuncs.com/renkx/v2ray:0.1
+
+  docker exec -it centosaiguo bash
 }
 
 update_sh() {
