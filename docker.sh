2e6e21f371b5927050364b553b7749142be62d0c
liunian <liunian@kdd.com>
2020-12-20 04:29:02 +0800

    update

--- docker.sh
+++ docker.sh
@@ -24,36 +24,7 @@ Error="${Red}[错误]${Font}"
 shell_version="0.1.0"
 # git分支
 github_branch="main"
-version_cmp="/tmp/version_cmp.tmp"
-v2ray_conf_dir="/etc/v2ray"
-nginx_conf_dir="/etc/nginx/conf/conf.d"
-v2ray_conf="${v2ray_conf_dir}/config.json"
-nginx_conf="${nginx_conf_dir}/v2ray.conf"
-nginx_dir="/etc/nginx"
-web_dir="/home/wwwroot"
-nginx_openssl_src="/usr/local/src"
-v2ray_bin_dir_old="/usr/bin/v2ray"
-v2ray_bin_dir="/usr/local/bin"
-v2ray_info_file="$HOME/v2ray_info.inf"
-v2ray_qr_config_file="/usr/local/vmess_qr.json"
-nginx_systemd_file="/etc/systemd/system/nginx.service"
-v2ray_systemd_file="/etc/systemd/system/v2ray.service"
-v2ray_access_log="/var/log/v2ray/access.log"
-v2ray_error_log="/var/log/v2ray/error.log"
-amce_sh_file="/root/.acme.sh/acme.sh"
-ssl_update_file="/usr/bin/ssl_update.sh"
-nginx_version="1.18.0"
-openssl_version="1.1.1g"
-jemalloc_version="5.2.1"
-old_config_status="off"
-# v2ray_plugin_version="$(wget -qO- "https://github.com/shadowsocks/v2ray-plugin/tags" | grep -E "/shadowsocks/v2ray-plugin/releases/tag/" | head -1 | sed -r 's/.*tag\/v(.+)\">.*/\1/')"
-
-#简易随机数
-random_num=$((RANDOM%12+4))
-#生成伪装路径
-camouflage="/$(head -n 10 /dev/urandom | md5sum | head -c ${random_num})/"
-
-THREAD=$(grep 'processor' /proc/cpuinfo | sort -u | wc -l)
+version_cmp="/tmp/docker_sh_version_cmp.tmp"
 
 #获取系统相关参数
 #NAME="CentOS Linux"
@@ -223,6 +194,7 @@ dependency_install() {
         #       judge "haveged 启动"
     fi
 }
+
 basic_optimization() {
     # 最大文件打开数
     sed -i '/^\*\ *soft\ *nofile\ *[[:digit:]]*/d' /etc/security/limits.conf
@@ -253,7 +225,9 @@ install_docker() {
 }
 
 update_sh() {
+    # 获取新的版本号
     ol_version=$(curl -L -s https://raw.githubusercontent.com/rkxs/s/${github_branch}/docker.sh | grep "shell_version=" | head -1 | awk -F '=|"' '{print $3}')
+    # 版本比较
     echo "$ol_version" >$version_cmp
     echo "$shell_version" >>$version_cmp
     if [[ "$shell_version" < "$(sort -rV $version_cmp | head -1)" ]]; then
@@ -261,7 +235,7 @@ update_sh() {
         read -r update_confirm
         case $update_confirm in
         [yY][eE][sS] | [yY])
-            wget -N --no-check-certificate https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/${github_branch}/install.sh
+            wget -N --no-check-certificate https://raw.githubusercontent.com/rkxs/s/${github_branch}/docker.sh
             echo -e "${OK} ${GreenBG} 更新完成 ${Font}"
             exit 0
             ;;
@@ -271,113 +245,27 @@ update_sh() {
     else
         echo -e "${OK} ${GreenBG} 当前版本为最新版本 ${Font}"
     fi
-
 }
 
 menu() {
     update_sh
-    echo -e "\t V2ray 安装管理脚本 ${Red}[${shell_version}]${Font}"
-    echo -e "\t---authored by wulabing---"
-    echo -e "\thttps://github.com/wulabing\n"
-    echo -e "当前已安装版本:${shell_mode}\n"
+    echo -e "\t docker 安装脚本 ${Red}[${shell_version}]${Font}"
+    echo -e "\thttps://git.io/renkx\n"
 
     echo -e "—————————————— 安装向导 ——————————————"""
-    echo -e "${Green}0.${Font}  升级 脚本"
     echo -e "${Green}1.${Font}  安装 V2Ray (Nginx+ws+tls)"
-    echo -e "${Green}2.${Font}  安装 V2Ray (http/2)"
-    echo -e "${Green}3.${Font}  升级 V2Ray core"
-    echo -e "—————————————— 配置变更 ——————————————"
-    echo -e "${Green}4.${Font}  变更 UUID"
-    echo -e "${Green}5.${Font}  变更 alterid"
-    echo -e "${Green}6.${Font}  变更 port"
-    echo -e "${Green}7.${Font}  变更 TLS 版本(仅ws+tls有效)"
-    echo -e "—————————————— 查看信息 ——————————————"
-    echo -e "${Green}8.${Font}  查看 实时访问日志"
-    echo -e "${Green}9.${Font}  查看 实时错误日志"
-    echo -e "${Green}10.${Font} 查看 V2Ray 配置信息"
-    echo -e "—————————————— 其他选项 ——————————————"
-    echo -e "${Green}11.${Font} 安装 4合1 bbr 锐速安装脚本"
-    echo -e "${Green}12.${Font} 安装 MTproxy(支持TLS混淆)"
-    echo -e "${Green}13.${Font} 证书 有效期更新"
-    echo -e "${Green}14.${Font} 卸载 V2Ray"
-    echo -e "${Green}15.${Font} 更新 证书crontab计划任务"
-    echo -e "${Green}16.${Font} 清空 证书遗留文件"
-    echo -e "${Green}17.${Font} 退出 \n"
+    echo -e "${Green}2.${Font} 安装 4合1 bbr 锐速安装脚本"
+    echo -e "${Green}3.${Font} 退出 \n"
 
     read -rp "请输入数字：" menu_num
     case $menu_num in
-    0)
-        update_sh
-        ;;
     1)
-        shell_mode="ws"
-        install_v2ray_ws_tls
+        install_docker
         ;;
     2)
-        shell_mode="h2"
-        install_v2_h2
-        ;;
-    3)
-        bash <(curl -L -s https://raw.githubusercontent.com/wulabing/V2Ray_ws-tls_bash_onekey/${github_branch}/v2ray.sh)
-        ;;
-    4)
-        read -rp "请输入UUID:" UUID
-        modify_UUID
-        start_process_systemd
-        ;;
-    5)
-        read -rp "请输入alterID:" alterID
-        modify_alterid
-        start_process_systemd
-        ;;
-    6)
-        read -rp "请输入连接端口:" port
-        if grep -q "ws" $v2ray_qr_config_file; then
-            modify_nginx_port
-        elif grep -q "h2" $v2ray_qr_config_file; then
-            modify_inbound_port
-        fi
-        start_process_systemd
-        ;;
-    7)
-        tls_type
-        ;;
-    8)
-        show_access_log
-        ;;
-    9)
-        show_error_log
-        ;;
-    10)
-        basic_information
-        if [[ $shell_mode == "ws" ]]; then
-            vmess_link_image_choice
-        else
-            vmess_qr_link_image
-        fi
-        show_information
-        ;;
-    11)
         bbr_boost_sh
         ;;
-    12)
-        mtproxy_sh
-        ;;
-    13)
-        stop_process_systemd
-        ssl_update_manuel
-        start_process_systemd
-        ;;
-    14)
-        uninstall_all
-        ;;
-    15)
-        acme_cron_update
-        ;;
-    16)
-        delete_tls_key_and_crt
-        ;;
-    17)
+    3)
         exit 0
         ;;
     *)
@@ -386,5 +274,4 @@ menu() {
     esac
 }
 
-judge_mode
-list "$1"
+menu
