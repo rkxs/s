f4eaed75f3bd3aedd5f4fd10ec30b5257a88419b
liunian <liunian@kdd.com>
2020-12-20 08:21:55 +0800

    main

--- docker.sh
+++ docker.sh
@@ -239,7 +239,7 @@ install_docker() {
 
 docker_run() {
 
-  docker run -itd --name centosaiguo -p 80:80 -p 443:443 --privileged=true --restart=always registry.cn-beijing.aliyuncs.com/renkx/v2ray:0.1
+  docker run -itd --name centosaiguo -p 80:80 -p 443:443 --privileged=true --restart=always registry.cn-beijing.aliyuncs.com/renkx/v2ray:v0.1
 
   docker exec -it centosaiguo bash
 }
