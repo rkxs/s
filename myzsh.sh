6d7c24fd6e7237ab2346bfb4bc22d16a993afa9a
liunian <liunian@kdd.com>
2020-12-23 10:47:48 +0800

    update

--- myzsh.sh
+++ myzsh.sh
@@ -1,10 +1,53 @@
 #!/bin/sh
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+cd "$(
+    cd "$(dirname "$0")" || exit
+    pwd
+)" || exit
 
 zshrc_file="~/.zshrc"
 
-yum -y install wget zsh
+command_exists() {
+	command -v "$@" >/dev/null 2>&1
+}
+
+fmt_error() {
+  echo ${RED}"Error: $@"${RESET} >&2
+}
+
+setup_color() {
+	# Only use colors if connected to a terminal
+	if [ -t 1 ]; then
+		RED=$(printf '\033[31m')
+		GREEN=$(printf '\033[32m')
+		YELLOW=$(printf '\033[33m')
+		BLUE=$(printf '\033[34m')
+		BOLD=$(printf '\033[1m')
+		RESET=$(printf '\033[m')
+	else
+		RED=""
+		GREEN=""
+		YELLOW=""
+		BLUE=""
+		BOLD=""
+		RESET=""
+	fi
+}
+
+setup_color
+
+command_exists git || {
+    fmt_error "git is not installed"
+    exit 1
+}
+
+if ! command_exists zsh; then
+    yum -y install zsh
+fi
 
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
 
 # 替换主题
-sed -i 's/ZSH_THEME=.*/ZSH_THEME="ys"/' ./zshrc
+[ -f "~/.zshrc" ] && sed -i 's/ZSH_THEME=.*/ZSH_THEME="ys"/' ~/.zshrc
